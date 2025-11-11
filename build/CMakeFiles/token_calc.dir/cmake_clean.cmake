9f2aedaefbfad27a36ed7c7cb49fcd76e404a848
liboxin-cloud <3551839250@qq.com>
2025-11-11 23:53:25 +0800

    continue coding

---
+++ build/CMakeFiles/token_calc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/token_calc.dir/parser.c.o"
+  "CMakeFiles/token_calc.dir/parser.c.o.d"
+  "CMakeFiles/token_calc.dir/test.c.o"
+  "CMakeFiles/token_calc.dir/test.c.o.d"
+  "token_calc"
+  "token_calc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/token_calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
